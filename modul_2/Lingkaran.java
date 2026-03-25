c2a8f00e431c791b63d79c257e8297f7bf030d65
sbrinA-MSG <mymarketsbrn.437@gmail.com>
2026-03-25 20:39:42 +0700

    Add Lingkaran class for circle calculations

---
+++ modul_2/Lingkaran.java
@@ -0,0 +1,14 @@
+public class Lingkaran {
+    public static float PI = 3.14f;
+    public float r;
+    public Lingkaran(float r) {
+        this.r = r;
+    }
+    public float luas() {
+        return PI * r * r;
+    }
+
+    public static float keliling(float r){
+        return 2*PI*r;
+    }
+}
