7081d3542473379613365bd2eca1c9badc50b258
sbrinA-MSG <mymarketsbrn.437@gmail.com>
2026-03-25 23:54:01 +0700

    Update GeometriApp.java

--- modul_2/GeometriApp.java
+++ modul_2/GeometriApp.java
@@ -4,8 +4,14 @@ public class GeometriApp {
       Lingkaran lingBesar = new Lingkaran(11.0f);
       float lK = lingKecil.luas();
       float lB = lingBesar.luas();
+        /*
+        Memanggil static method keliling() dari class Lingkaran.
+        Dipanggil melalui nama class (Lingkaran.keliling), bukan melalui objek,
+        tidak bisa mengakses atribut non-static 'r' secara langsung.
+        */
       float kK = Lingkaran.keliling(lingKecil.r);
       float kB = Lingkaran.keliling(lingBesar.r);
+        //Menampilkan hasil keliling lingkaran
       System.out.println("Keliling Lingkaran Kecil: " + kK);
       System.out.println("Keliling Lingkaran Besar: " + kB);
       System.out.println("Luas Lingkaran Kecil: " + lK);
