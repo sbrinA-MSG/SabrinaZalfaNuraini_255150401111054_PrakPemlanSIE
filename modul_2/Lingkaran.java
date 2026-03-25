4822006bd718d6cd31e0c425b07374737be172c8
sbrinA-MSG <mymarketsbrn.437@gmail.com>
2026-03-25 23:51:51 +0700

    Update Lingkaran.java

--- modul_2/Lingkaran.java
+++ modul_2/Lingkaran.java
@@ -1,13 +1,21 @@
 public class Lingkaran {
+    //Dengan static, PI bisa diakses langsung melalui Lingkaran.PI tanpa perlu membuat objek.
     public static float PI = 3.14f;
+    //Non-static berarti setiap objek Lingkaran memiliki nilai r-nya masing-masing.
     public float r;
     public Lingkaran(float r) {
         this.r = r;
     }
+    //Karena menggunakan atribut non-static 'r', method luas dipanggil melalui objek.
     public float luas() {
         return PI * r * r;
     }
 
+    /*
+    Method static keliling() untuk menghitung keliling lingkaran.
+    Bersifat static sehingga bisa dipanggil langsung via Lingkaran.keliling(r) tanpa objek.
+    Menerima parameter float r karena static method tidak bisa mengakses atribut non-static 'r' secara langsung.
+    */
     public static float keliling(float r){
         return 2*PI*r;
     }
