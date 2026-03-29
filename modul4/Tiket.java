265b22ae86f008707cde23d0911a5ca71c565650
sbrinA-MSG <mymarketsbrn.437@gmail.com>
2026-03-29 23:58:22 +0700

    Add Tiket class with ticket booking methods

---
+++ modul4/Tiket.java
@@ -0,0 +1,48 @@
+public class Tiket {
+    protected String namaPenumpang = "Anonim";
+    protected String tujuan = "Jakarta";
+    protected int jarakTempuh = 750;
+    protected double hargaTiket = 250000;
+
+    public void pesan() {}
+
+    public void pesan(String n, String t) {
+        this.namaPenumpang = n;
+        this.tujuan = t;
+        this.jarakTempuh = 90; this.hargaTiket = 55000;
+    }
+
+    public void pesan(String n, String t, int j, double h) {
+        this.namaPenumpang = n;
+        this.tujuan = t;
+        this.jarakTempuh = j; 
+        this.hargaTiket = h;
+    }
+
+    /*
+    no.5A
+    Method overlad pesan (String, int)
+    parameter: namaPenumpang dan jarakTempuh
+    */
+    public void pesan(String n, int j){
+        this.namaPenumpang = n;
+        this.jarakTempuh = j;
+    }
+
+    /*
+    no.6A
+    percobaan method overload pesan(String tujuan, String nama)
+    
+    public void pesan (String t, String n){
+        this.tujuan = t;
+        this.namaPenumpang = n;
+    }
+    */
+    public void tampil() {
+        System.out.println("Nama Penumpang: " + this.namaPenumpang);
+        System.out.println("Tujuan: " + this.tujuan);
+        System.out.println("Jarak Tempuh: " + this.jarakTempuh);
+        System.out.println("Harga Tiket: " + this.hargaTiket);
+        System.out.println("--------------------");
+    }
+}
